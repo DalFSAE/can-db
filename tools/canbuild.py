8f46d85ac3598a320249eb60f4620ed9298ccfe1
Ashton Dudley <ashtondudley03@gmail.com>
2025-12-30 12:39:31 -0400

    started build process

--- tools/canbuild.py
+++ tools/canbuild.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
 import sys
+import cantools
 from pathlib import Path
 
+SCRIPT_DIR = Path(__file__).resolve().parent
+ROOT = SCRIPT_DIR.parent
+BUILD = ROOT / "build"
+
+INPUTS = [
+    ROOT / "networks" / "can0_powertrain.dbc",
+    ROOT / "networks" / "can1_data.dbc",
+]
+
+def debug_info():
+    print(SCRIPT_DIR)
+    print(ROOT)
 
 def build():
     print("Starting build process...")
+    BUILD.mkdir(exist_ok=True) 
 
 def lint():
     print("Starting lint process...")
@@ -29,4 +43,6 @@ def main():
         clean()
 
 if __name__ == "__main__":
+    debug_info()
     main()
+
