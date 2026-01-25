8dae58be89be00512c7d175460f6a81b5aa8eb94
ADUDLEY <ashtondudley03@gmail.com>
2026-01-25 18:10:05 -0400

    confirmed testing works `ctest --test-dir build`

--- tests/test_main.c
+++ tests/test_main.c
@@ -3,7 +3,12 @@
 void setUp(void) {}
 void tearDown(void) {}
 
+void test_alive(void) {
+    TEST_ASSERT_EQUAL(1, 1);
+}
+
 int main(void) {
     UNITY_BEGIN();
+    RUN_TEST(test_alive);
     return UNITY_END();
 }
